0dbdb16c8fe4bbd38ea93cdb7fda5e5d1c8403ff
stenjansarv <stenjansarv@gmail.com>
2020-10-22 18:28:50 +0300

    Updates

---
+++ precache-manifest.6f5c9da6a295a633b92454a9536cec82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc9b0f4ceba015aae446041c0a82f42d",
+    "url": "/onlineLab/index.html"
+  },
+  {
+    "revision": "4e137382219c542cb7e4",
+    "url": "/onlineLab/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97844ce8f136d81800bd",
+    "url": "/onlineLab/static/js/2.a84c0988.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/onlineLab/static/js/2.a84c0988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e137382219c542cb7e4",
+    "url": "/onlineLab/static/js/main.dcb9eeea.chunk.js"
+  },
+  {
+    "revision": "689eb99185d0cfba2bfe",
+    "url": "/onlineLab/static/js/runtime-main.199c0fba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/onlineLab/static/media/logo.5d5d9eef.svg"
+  }
+]);
