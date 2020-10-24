5207cb646a108f0a3edadf805f2dc920f2d55770
stenjansarv <stenjansarv@gmail.com>
2020-10-24 11:17:30 +0300

    Add react bootstrap and a demo navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import NavigationBar from './components/NavigationBar'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <NavigationBar />
     </div>
   );
 }
