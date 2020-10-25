f8c98fa0a256b23da6999d3f11143a1bda8a4868
stenjansarv <stenjansarv@gmail.com>
2020-10-25 15:30:48 +0200

    Create about and contact page

---
+++ src/Pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div style={{  
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat'
+    }}>
+    </div>
+  );
+}
+
+export default About
