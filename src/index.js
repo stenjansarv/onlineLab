5e3ad008955927141098f1fd5e25f55d31dbc138
stenjansarv <stenjansarv@gmail.com>
2021-03-09 22:24:59 +0200

    refactor: refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+// import './index.css' // TODO: Add this back once not using antd anymore
+import 'antd/dist/antd.css'
 import App from './App'
 
+// Redux
+import { Provider } from 'react-redux';
+import configureStore, { history } from './redux/store';
+import { ConnectedRouter } from 'connected-react-router/immutable'
+
+const store = configureStore()
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+        <App />
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById('root')
 )
